e22ca844994d78010b8c386c970feb0a32be0079
SYnx143 <tekakito143@gmail.com>
2024-11-24 07:20:57 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function changeToMaleStyle() {
+    document.body.style.backgroundColor = "blue";
+    document.body.style.color = "white";
+    document.body.style.fontFamily = "Arial, sans-serif";
+}
+
+function changeToFemaleStyle() {
+    document.body.style.backgroundColor = "pink";
+    document.body.style.color = "darkblue";
+    document.body.style.fontFamily = "Georgia, serif";
+}
+
+function changeToOldStyle() {
+document.body.style.fontSize = "40px"; 
+document.body.style.backgroundColor = "brown";
+document.body.style.color = "white";
+}
